5eca2f84e954e075772634b82688d38c00337db9
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2021-03-10 15:26:06 -0300

    segundo TESTE

--- src/main/java/br/com/treino/frameworks/teste/Teste.java
+++ src/main/java/br/com/treino/frameworks/teste/Teste.java
@@ -3,7 +3,8 @@ package br.com.treino.frameworks.teste;
 public class Teste {
 
     public void teste() {
-        System.out.println("TESTE");
+        System.out.println("TESTE1");
+        System.out.println("TESTE2");
     }
 
 }
