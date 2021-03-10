786f0f082c8e1a4bd54332d5b26cd0e51bb3079f
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2021-03-10 15:15:28 -0300

    Adicionei import e anotação extent

--- src/main/java/br/com/treino/frameworks/utlis/SextoBasicoBaseTest.java
+++ src/main/java/br/com/treino/frameworks/utlis/SextoBasicoBaseTest.java
@@ -3,9 +3,12 @@ package br.com.treino.frameworks.utlis;
 import br.com.treino.frameworks.webdrivers.Browser;
 import br.com.treino.frameworks.webdrivers.DriverFactory;
 import br.com.treino.frameworks.webdrivers.DriverManager;
+import com.aventstack.extentreports.testng.listener.ExtentITestListenerClassAdapter;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 
+@Listeners({ExtentITestListenerClassAdapter.class, Report.class})
 public class SextoBasicoBaseTest extends DriverFactory {
 
     @BeforeMethod
